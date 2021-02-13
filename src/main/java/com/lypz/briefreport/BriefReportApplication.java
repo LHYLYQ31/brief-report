1e525194c418968cb3c0b1af9ccb3ee3c43a9e08
LHULYQ31 <lhy31lyq@126.com>
2021-02-13 12:14:56 +0800

    初始化项目

---
+++ src/main/java/com/lypz/briefreport/BriefReportApplication.java
@@ -0,0 +1,13 @@
+package com.lypz.briefreport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BriefReportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BriefReportApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lypz/briefreport/BriefReportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lypz.briefreport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BriefReportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
